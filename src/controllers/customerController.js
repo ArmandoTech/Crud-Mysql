d8d4ef309e43d9f590799bfdb4662440d040a976
Armando Arenas <armandoarenass@hotmail.com>
2022-08-24 16:14:29 -0500

    delete options working

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,6 +1,6 @@
 const controller= {}
 
-controller.list= (req, res) => {
+controller.read= (req, res) => {
     req.getConnection((err, conn) => {
         conn.query('SELECT * FROM customer', (err, rows) => {
             if (err) {
@@ -19,8 +19,24 @@ controller.save= (req, res) => {
     
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO customer set ?', [data], (err, rows) => {
-            console.log(rows)
-            res.send('works')
+            if (err) {
+                res.json(err)
+            } else {
+                res.redirect('/')
+            }
+        })
+    })
+}
+
+controller.delete= (req, res) => {
+    const { id }= req.params
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM customer WHERE id= ?', [id], (err, rows) => {
+            if (err) {
+                res.json(err)
+            } else {
+                res.redirect('/')
+            }
         })
     })
 }
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,7 +3,8 @@ const router= express.Router()
 
 const customerController= require('../controllers/customerController.js')
 
-router.get('/', customerController.list)
+router.get('/', customerController.read)
 router.post('/add', customerController.save)
+router.get('/delete/:id', customerController.delete)
 
 module.exports= router
